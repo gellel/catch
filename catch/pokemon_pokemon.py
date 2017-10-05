627bfbfed4ef18305c7b83b3151ea33673bf7137
gellel <lindsay.gelle@gmail.com>
2017-10-05 19:55:20 +1100

    Project clean-up.

---
+++ catch/pokemon_pokemon.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+# -*- coding: utf-8 -*- 
+
+# from collections import named tuple module.
+from collections import namedtuple
+# import random
+import random
+# import system
+import sys
+# import pokemon_types
+from pokemon_types import *
+# import pokemon_moves
+from pokemon_moves import *
+
+########################
+### File information ###
+########################
+
+__author__  = "Lindsay Gelle (https://github.com/gellel)"
+
+__version__ = "1.0"
