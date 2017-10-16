1594f3197e08b6ac3fd99829e5fa39dc0e1a884f
gellel <lindsay.gelle@gmail.com>
2017-10-16 12:23:27 +1100

    Adding stats Hp and Speed builder.

--- catch/pokemon_base.py
+++ catch/pokemon_base.py
@@ -60,6 +60,7 @@ ARCHETYPE_KEY_STRS = " ".join([
 	
 def generate_training_class (pokemon_training):
 
+	# @return: @type: @class.__main__.Training
 	return namedtuple("Training", TRAINING_KEYS_STRS)(
 		EV_YIELD = generate_training_ev_yield_stats_class(*pokemon_training["EV_YIELD"]),
 		CATCH_RATE = generate_training_catch_rate_stats_class(*pokemon_training["CATCH_RATE"]),
@@ -69,6 +70,7 @@ def generate_training_class (pokemon_training):
 
 def generate_breeding_class (pokemon_breeding):
 
+	# @return: @type: @class.__main__.Breeding
 	return namedtuple("Breeding", BREEDING_KEYS_STRS)(
 		GROUPS = generate_breeding_groups_stats_class(pokemon_breeding["GROUPS"]), 
 		GENDER = generate_breeding_gender_stats_class(pokemon_breeding["GENDER"]), 
@@ -76,23 +78,28 @@ def generate_breeding_class (pokemon_breeding):
 
 def generate_stats_class (pokemon_stats):
 
+	# @return: @type: @class.__main__.Stats
 	return namedtuple("Stats", STATS_KEYS_STRS)(**{ 
 		key: namedtuple(re.sub(r'\s|_', "", str.title(key)), "BASE MIN MAX")(*value) 
 		for key, value in pokemon_stats.iteritems() })
 
-def generate_training_ev_yield_stats_class (pokemon_ev_yield_sum, pokemon_ev_yield_str):
+def generate_training_ev_yield_stats_class (pokemon_ev_yield_sum, 
+	pokemon_ev_yield_str):
 	""""""
 	# @return: @type: @class.__main__.EvYield
 	return namedtuple("EvYield", " ".join(["SUM", "STR"]))(
 		SUM = pokemon_ev_yield_sum, STR = pokemon_ev_yield_str)
 
-def generate_training_catch_rate_stats_class (pokemon_catch_rate_sum, pokemon_catch_rate_hp_rate, pokemon_catch_rate_ball):
+def generate_training_catch_rate_stats_class (pokemon_catch_rate_sum, 
+	pokemon_catch_rate_hp_rate, pokemon_catch_rate_ball):
 	""""""
 	# @return: @type: @class.__main__.Catch
 	return namedtuple("Catch", " ".join(["SUM", "HP_RATE", "POKEBALL"]))(
-		SUM = pokemon_catch_rate_sum, HP_RATE = pokemon_catch_rate_hp_rate, POKEBALL = pokemon_catch_rate_ball)
+		SUM = pokemon_catch_rate_sum, HP_RATE = pokemon_catch_rate_hp_rate, 
+			POKEBALL = pokemon_catch_rate_ball)
 
-def generate_training_happiness_stats_class (pokemon_happiness_sum, pokemon_happiness_str):
+def generate_training_happiness_stats_class (pokemon_happiness_sum, 
+	pokemon_happiness_str):
 	""""""
 	# @return: @type: @class.__main__.Happiness
 	return namedtuple("Happiness", " ".join(["SUM", "STR"]))(
@@ -112,46 +119,74 @@ def generate_training_growth_rate_stats_class (pokemon_growth_rate_str):
 	
 def generate_breeding_groups_stats_class (pokemon_groups_sequence):
 
-	groups = { pokemon_groups_sequence[i][0]: namedtuple("Props", " ".join(["SUM"]))(SUM = pokemon_groups_sequence[i][1])
-		for i in range(0, len(pokemon_groups_sequence)) }
+	groups = { pokemon_groups_sequence[i][0]: namedtuple("Props", " ".join(["SUM"]))(
+		SUM = pokemon_groups_sequence[i][1]) for i in range(0, len(pokemon_groups_sequence)) }
 	
 	# @return: @type: @class.__main__.Groups
 	return namedtuple("Groups", " ".join(dict.keys(groups)))(**groups)
 
 def generate_breeding_gender_stats_class (pokemon_gender_sequence):
 
-	groups = { pokemon_gender_sequence[i][0]: namedtuple("Props", " ".join(["SUM"]))(SUM = pokemon_gender_sequence[i][1])
-		for i in range(0, len(pokemon_gender_sequence)) }
+	groups = { pokemon_gender_sequence[i][0]: namedtuple("Props", " ".join(["SUM"]))(
+		SUM = pokemon_gender_sequence[i][1]) for i in range(0, len(pokemon_gender_sequence)) }
 	
 	# @return: @type: @class.__main__.Gender
 	return namedtuple("Gender", " ".join(dict.keys(groups)))(**groups)
 
-def generate_breeding_egg_cycles_stats_class (pokemon_egg_cycles_sum, pokemon_egg_cycles_steps):
+def generate_breeding_egg_cycles_stats_class (pokemon_egg_cycles_sum, 
+	pokemon_egg_cycles_steps):
 
 	# @return: @type: @class.__main__.EggCycles
 	return namedtuple("Eggs", " ".join(["SUM", "STEPS"]))(
 		SUM = pokemon_egg_cycles_sum, STEPS = pokemon_egg_cycles_steps)
 
-def generate_stats_attack_stats_class (pokemon_attack_stats_base, pokemon_attack_stats_min, pokemon_attack_stats_max):
+def generate_stats_hp_stats_class (pokemon_hp_stats_base, 
+		pokemon_hp_stats_min, pokemon_hp_stats_max):
 
+	# @return: @type: @class.__main__.Hp
+	return namedtuple("Hp", " ".join(["BASE", "MIN", "MAX"]))(
+		BASE = pokemon_hp_stats_base, MIN = pokemon_hp_stats_min, 
+			MAX = pokemon_hp_stats_max)
+
+def generate_stats_attack_stats_class (pokemon_attack_stats_base, 
+		pokemon_attack_stats_min, pokemon_attack_stats_max):
+
+	# @return: @type: @class.__main__.Attack
 	return namedtuple("Attack", " ".join(["BASE", "MIN", "MAX"]))(
-		BASE = pokemon_attack_stats_base, MIN = pokemon_attack_stats_min, MAX = pokemon_attack_stats_max)
+		BASE = pokemon_attack_stats_base, MIN = pokemon_attack_stats_min, 
+			MAX = pokemon_attack_stats_max)
 
-def generate_stats_defense_stats_class (pokemon_defense_stats_base, pokemon_defense_stats_min, pokemon_defense_stats_max):
-
-	return namedtuple("Defense", " ".join(["BASE", "MIN", "MAX"]))(
-		BASE = pokemon_defense_stats_base, MIN = pokemon_defense_stats_min, MAX = pokemon_defense_stats_max)
+def generate_stats_defense_stats_class (pokemon_defense_stats_base, 
+		pokemon_defense_stats_min, pokemon_defense_stats_max):
 	
-def generate_stats_sp_attack_stats_class (pokemon_sp_attack_stats_base, pokemon_sp_attack_stats_min, pokemon_sp_attack_stats_max):
+	# @return: @type: @class.__main__.Defense
+	return namedtuple("Defense", " ".join(["BASE", "MIN", "MAX"]))(
+		BASE = pokemon_defense_stats_base, MIN = pokemon_defense_stats_min, 
+			MAX = pokemon_defense_stats_max)
+	
+def generate_stats_sp_attack_stats_class (pokemon_sp_attack_stats_base, 
+		pokemon_sp_attack_stats_min, pokemon_sp_attack_stats_max):
 
+	# @return: @type: @class.__main__.SpAttack
 	return namedtuple("SpAttack", " ".join(["BASE", "MIN", "MAX"]))(
-		BASE = pokemon_sp_attack_stats_base, MIN = pokemon_sp_attack_stats_min, MAX = pokemon_sp_attack_stats_max)
+		BASE = pokemon_sp_attack_stats_base, MIN = pokemon_sp_attack_stats_min, 
+			MAX = pokemon_sp_attack_stats_max)
 
-def generate_stats_sp_defense_stats_class (pokemon_sp_defense_stats_base, pokemon_sp_defense_stats_min, pokemon_sp_defense_stats_max):
+def generate_stats_sp_defense_stats_class (pokemon_sp_defense_stats_base, 
+		pokemon_sp_defense_stats_min, pokemon_sp_defense_stats_max):
 
+	# @return: @type: @class.__main__.SpDefense
 	return namedtuple("SpDefense", " ".join(["BASE", "MIN", "MAX"]))(
-		BASE = pokemon_sp_defense_stats_base, MIN = pokemon_sp_defense_stats_min, MAX = pokemon_sp_defense_stats_max)
+		BASE = pokemon_sp_defense_stats_base, MIN = pokemon_sp_defense_stats_min, 
+			MAX = pokemon_sp_defense_stats_max)
 
+def generate_stats_speed_stats_class (pokemon_speed_stats_base, 
+		pokemon_speed_stats_min, pokemon_speed_stats_max):
+
+	# @return: @type: @class.__main__.Hp
+	return namedtuple("Speed", " ".join(["BASE", "MIN", "MAX"]))(
+		BASE = pokemon_speed_stats_base, MIN = pokemon_speed_stats_min, 
+			MAX = pokemon_speed_stats_max)
 
 ######################
 ### Module classes ###
